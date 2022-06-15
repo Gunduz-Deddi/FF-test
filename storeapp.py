85a59ea2b7db7925af878d75372da9358b5be028
Gunduz-Deddi <deyzi712@mail.ru>
2022-06-15 13:57:39 +0400

    sdgrddrtrtrtrt

--- storeapp.py
+++ storeapp.py
@@ -3,4 +3,13 @@ from PySide6.QtGui import *
 from PySide6.QtCore import *
 import sqlite3
 
+app=QApplication
+
+class Store(Qwidget):
+    def__init__(self):
+        super.__(init)__()
+        self.resize(600,600).
+    
+
+
 
